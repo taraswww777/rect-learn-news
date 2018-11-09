b43e14966b66e9adf8cc9f0b404867b26359429e
Taras_Kovalev <taraswww777@mail.ru>
2018-11-09 12:40:54 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import logo from '../../logo.svg';
+import './App.scss';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="app">
+				<header className="app__header">
+					<div className="app__container">
+						<div className="app__row">
+							<div className="app__logo">
+
+							<img src={logo} className="app__logo-img" alt="logo"/>
+							</div>
+							<div className={'app__title'}>List news</div>
+						</div>
+					</div>
+				</header>
+
+				<main className={'app__main'}>
+					{this.props.children}
+				</main>
+
+				<footer className={'app__footer'}>
+					<div className="app__container">
+						<div className="app__row">
+							task for test
+						</div>
+					</div>
+				</footer>
+			</div>
+		);
+	}
+}
+
+export default App;
